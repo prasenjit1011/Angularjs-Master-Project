1996e33543d72d84301f8479111c621eb13f23b0
Prasenjit Aluni <prasenjit.aluni@codeclouds.com>
2023-12-09 20:52:18 +0530

    01: Create Component : ng g c product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ProductFrmComponent } from './product-frm/product-frm.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailsComponent } from './product/product-details/product-details.component';
+import { ProductFrmComponent } from './product/product-frm/product-frm.component';
 
 @NgModule({
   declarations: [
