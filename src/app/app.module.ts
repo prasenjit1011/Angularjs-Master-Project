b7341d9fba64ecf7d3aed55adab47ee2487ce421
Riyan <riyan04314@gmail.com>
2024-01-29 21:41:19 +0530

    Navbar & TickerTape component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TickerTapeComponent } from './ticker-tape/ticker-tape.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoute = [
+  {path : '', component:TickerTapeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TickerTapeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
