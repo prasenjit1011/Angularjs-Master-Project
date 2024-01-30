212977749cbdd355f30ab46ca1b952b7af29144d
Riyan <riyan04314@gmail.com>
2024-01-30 17:55:23 +0530

    Tickertape

--- src/app/components/ticker-tape/ticker-tape.component.ts
+++ src/app/components/ticker-tape/ticker-tape.component.ts
@@ -8,16 +8,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TickerTapeComponent {
   ltpdata = undefined;
-  apiHost = 'http://localhost/myproject/Api';
+  //apiHost = 'http://localhost:3000';
+  apiHost = 'https://mmsv6q-3000.csb.app';
 
   constructor(private http: HttpClient){
-    //let apiUrlz = 'https://quotes-api.tickertape.in/quotes?sids=IRE,INOXI,HAP,ADAG,ESAF,IRM,SULA,ADEL,DABU,WIPR,IGAS,CAMP,ADAN,KTKM,RELI,ADAI,APTU,AET,INFY,BION';
-    let apiUrl = this.apiHost+'/ltpdata.php';
+    let apiUrl = this.apiHost+'/stock/list';
+
     this.http
         .get(apiUrl)
         .subscribe(data=>(
           console.log(data),
-          this.ltpdata = data
+          this.ltpdata = data['apiData']
         ));
 
   }
