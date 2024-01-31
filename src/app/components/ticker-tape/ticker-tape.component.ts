02b1904ed0cbda7b6a1abe649faf5b44a8c72641
Riyan <riyan04314@gmail.com>
2024-01-31 10:00:52 +0530

    Tickertape

--- src/app/components/ticker-tape/ticker-tape.component.ts
+++ src/app/components/ticker-tape/ticker-tape.component.ts
@@ -7,20 +7,33 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './ticker-tape.component.css'
 })
 export class TickerTapeComponent {
+  
+  profit  = 0;
   ltpdata = undefined;
   sidData = undefined;
   apiHost = 'http://localhost:3000';
+  sandboxHost = 'https://ynhsgq-3000.csb.app'
 
   constructor(private http: HttpClient){
-    let apiUrl = this.apiHost+'/stock/list';
+    let apiUrl = this.sandboxHost+'/stock/list';
 
     this.http
         .get(apiUrl)
         .subscribe(data=>(
-          console.log(data['sidData']['ADAG']),
           this.sidData  = data['sidData'],
-          this.ltpdata  = data['apiData']
+          this.ltpdata  = data['apiData'],
+          this.mydata()
         ));
+  }
+
+  mydata() {
+
+    console.log('-----here-----------');
+    this.ltpdata.map((val,key)=>{
+      this.profit += this.sidData[val['sid']]['cqty']*val['dyChange']*val['c']*0.01;
+    });
+     
 
   }
+
 }
