2149322b949567e0da0d0e5caa11d8dbdde719e0
Riyan <riyan04314@gmail.com>
2024-02-27 08:01:59 +0530

    Tickertape

--- src/app/components/stock-details/stock-details.component.ts
+++ src/app/components/stock-details/stock-details.component.ts
@@ -73,7 +73,7 @@ export class StockDetailsComponent {
 
   private handleError(error:HttpErrorResponse) {
     if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
+      //// A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
     } else {
       // The backend returned an unsuccessful response code.
