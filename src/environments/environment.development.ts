a033c90570888f23f2a82c0f9f484e061e88e2cd
Riyan <riyan04314@gmail.com>
2024-03-07 08:20:09 +0530

    Header Section

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     server: false,
-    apiHost: false ? 'http://localhost:3000' : 'https://ynhsgq-3000.csb.app'
+    apiHost: true ? 'http://localhost:3000' : 'https://ynhsgq-3000.csb.app'
 };
